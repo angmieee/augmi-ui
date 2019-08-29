e1e2ab22d10df4eb35622fe1e6d3879a0d6d2ebd
angmieee <firemao96@gmail.com>
2019-08-29 19:26:53 +0800

    添加加载状态

--- src/button.vue
+++ src/button.vue
@@ -2,6 +2,7 @@
 
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
         <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -31,6 +32,10 @@
 </script>
 
 <style lang="scss" scoped>
+    @keyframes spin {
+        0% {transform: rotate(0deg);}
+        100% {transform: rotate(360deg);}
+    }
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
@@ -70,6 +75,9 @@
             > .content{
                 order: 1;
             }
+        }
+        .loading{
+            animation: spin 1s infinite linear;
         }
     }
 
