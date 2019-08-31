0d78c2ec4115a2c6b29ef1b619176116ef1e156e
angmieee <firemao96@gmail.com>
2019-08-31 11:18:42 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="col" :class="`col-${span}`">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+        :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
+        <div style="border: 1px solid green;height: 100px;">
+            <slot></slot>
+        </div>
+
     </div>
 </template>
 
@@ -8,22 +12,36 @@
     export default {
         name: "Col",
         props:{
-            span:[Number,String]
+            span:[Number,String],
+            offset:[Number,String],
+
+        },
+        data(){
+            return {
+                gutter:0
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
     .col{
-        height: 100px;
+
         background: gray;
         width: 50%;
-        border: 1px solid red;
+        /*border: 1px solid red;*/
+        padding: 0 10px;
 
         $class-prefix: col-;
         @for $n from 1 through 24{
             &.#{$class-prefix}#{$n}{
                 width: ($n / 24)*100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n}{
+                margin-left: ($n / 24)*100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,29 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Row"
+        name: "Row",
+        props:{
+            gutter:[Number,String]
+        },
+        created() {
+        },
+        mounted() {
+            console.log(this.$children)
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+        }
     }
 </script>
 
 <style lang="scss" scoped>
     .row{
         display: flex;
+        margin: 0 -10px;
     }
 </style>
