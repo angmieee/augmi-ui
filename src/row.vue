45039ceb53d19a1a8d820422efbc9930066a0096
angmieee <firemao96@gmail.com>
2019-08-31 13:20:33 +0800

    完成 row 和 col 的测试

--- src/row.vue
+++ src/row.vue
@@ -1,32 +1,51 @@
 <template>
-    <div class="row" :style="rowStyle">
+    <div class="row" :style="rowStyle" :class="rowClass">
         <slot></slot>
     </div>
 </template>
-
 <script>
     export default {
-        name: "Row",
-        props:{
-            gutter:[Number,String]
-        },
-        computed:{
-            rowStyle(){
-                let {gutter} = this
-                return {marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}
+        name: 'Row',
+        props: {
+            gutter: {
+                type: [Number, String]
+            },
+            align: {
+                type: String,
+                validator (value) {
+                    return ['left', 'right', 'center'].indexOf(value) >= 0
+                }
             }
         },
-        mounted() {
-            this.$children.forEach((vm)=>{
+        computed: {
+            rowStyle () {
+                let {gutter} = this
+                return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+            },
+            rowClass () {
+                let {align} = this
+                return [align && `align-${align}`]
+            }
+        },
+        mounted () {
+            this.$children.forEach((vm) => {
                 vm.gutter = this.gutter
             })
         }
     }
 </script>
-
-<style lang="scss" scoped>
+<style scoped lang="scss">
     .row{
         display: flex;
-
+        flex-wrap: wrap;
+        &.align-left {
+            justify-content: flex-start;
+        }
+        &.align-right {
+            justify-content: flex-end;
+        }
+        &.align-center {
+            justify-content: center;
+        }
     }
 </style>
