26466a8e9fe3a398592d4d21f50ca793c703dda0
angmieee <firemao96@gmail.com>
2019-08-31 11:37:32 +0800

    重构 row 和 col

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
@@ -10,10 +10,13 @@
         props:{
             gutter:[Number,String]
         },
-        created() {
+        computed:{
+            rowStyle(){
+                let {gutter} = this
+                return {marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}
+            }
         },
         mounted() {
-            console.log(this.$children)
             this.$children.forEach((vm)=>{
                 vm.gutter = this.gutter
             })
@@ -24,6 +27,6 @@
 <style lang="scss" scoped>
     .row{
         display: flex;
-        margin: 0 -10px;
+
     }
 </style>
